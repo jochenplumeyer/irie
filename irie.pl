ebd4f223108ccc396da4cdc2269eebabc2fcff5e
Jochen Plumeyer <jochen@plumeyer.org>
2016-05-22 01:20:25 +0200

    First the concept, then the implementation.

--- irie.pl
+++ irie.pl
@@ -4,4 +4,13 @@ use strict;
 use YAML::XS;
 use Data::Dumper;
 
+my ($i, %a, $c);
+$i=0;
 
+$c=<<EndOfCode;
+Ich NameIst "Jochen".
+Ich GeborenAm "8.1.1968".
+Ding AttributName "Datum".
+EndOfCode
+
+print "This is the code:\n$c\n";
