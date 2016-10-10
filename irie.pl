0d4f5030242298c8db7073fbde328f72e0f441fd
Jochen Plumeyer <jochen@plumeyer.org>
2016-10-10 08:45:38 +0200

    Specification gathering "process"

--- irie.pl
+++ irie.pl
@@ -4,7 +4,7 @@ use strict;
 use YAML::XS;
 use Data::Dumper;
 use utf8;
-my (%h, %d);
+my (%h, %d, $key);
 
 # Objekte für den Beginn sollen sein:
 # Für UNIX-Systeme:
@@ -13,11 +13,26 @@ my (%h, %d);
 # 	Verzeichnis
 # 	Datei
 
-$d{"process"}{"name=de"}="Prozeß";
+$d{"process"}{"meta"}{"name"}{"de"}="Prozeß";
+$d{"process"}{"meta"}{"maincontext"}{"operating systems"}="UNIX";
+# Verbalisierung der Sätze:
+- Prozesse machen die eigentliche Arbeit eines Computers
+- hat eine eindeutige natürliche Zahl zugeordnet (also eine ID), die "process ID", ("pid").
+- im Verzeichnis /proc/$pid befinden sich alle Informationen über den Prozeß
+  - exe: Pfad des gestarteten Programms
+  - cmdline: Mit binären Nullen getrenntes Array con Kommandozeile und -Argumenten
+  - environ: Definition von Umgebungsvariablen
+  - fd: Verzeichnis mit Links zu den offenen Filehandles des Prozesses
+  - status: Systeminformationen zum Prozeß
 
 # Einhängen des temporären Datenbaumes in die globale
 # Hierarchie-Datenstruktur
-$h{"UNIX systems"}=\%d;
+#$h{"UNIX systems"}=\%d;
+#%h=%d;
+foreach $key (keys %d)
+{
+	$h{$key}=$d{$key};
+}
 # Differenz zu anderen, Nicht-UNIX Systemen
 # 
 # Danach würden hinzukommen:
